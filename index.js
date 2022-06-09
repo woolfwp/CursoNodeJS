9b27ec6fa5f1fcae692f5ecca38ae250b360665a
Sebastian Gomez <sebasgomez1099@gmail.com>
2022-06-09 10:00:50 -0500

    leyendo archivos con modulo FS importado de nodejs

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
-fs.writeFileSync('./textoPrueba.txt', 'Prueba modulos nodejs, creando archivo', function(err){
+fs.readFile('./textoPrueba.txt', function(err, data){
     if(err){
         console.log(err);
     }
-    console.log('Archivo creado satisfactoriamente');
+    console.log(data.toString());
 })
