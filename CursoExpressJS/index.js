5a115207aacf12721be6600e18d171f38054c436
Sebastian Gomez <sebasgomez1099@gmail.com>
2022-06-09 17:55:07 -0500

    fin 09/06/2022 practica con framework Express

--- CursoExpressJS/index.js
+++ CursoExpressJS/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) =>{
-    res.send('Prueba')
+    res.send('Peticion get recibida')
     res.end();
 });
+
+app.post('/aboute/:id', (req, res) =>{
+    res.send('post request received')
+    console.log(req.body);
+    console.log(req.params);
+})
+
+app.put('/contact', (req, res) =>{
+    res.send('put request received')
+})
+
+app.delete('/test/:userId', (req, res) =>{
+    res.send(`user ${req.params}`);
+})
 
 app.listen(3000, () =>{
     console.log('server on port 3000');
