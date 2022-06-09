fd7a0dd3503a18e89ee24127a01eac7d3de4735a
Sebastian Gomez <sebasgomez1099@gmail.com>
2022-06-09 10:58:01 -0500

    creando servidor con framework express

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const http = require('http');
 const colors = require('colors');
 
@@ -13,3 +14,18 @@ server.listen(3000, function(){
     console.log('Server on port 3000'.green)
 });
 
+*/
+const colors = require('colors');
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req,res) =>{
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.write('<h1>Hola Mundo</h1>');
+    res.end();
+})
+
+server.listen(3000, () =>{
+    console.log('server on port 3000'.green);
+});
